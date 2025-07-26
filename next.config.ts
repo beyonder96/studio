85b51f9df752c96352ef98b2096ba4d603c5ce81
Kenned Ferreira <kennedoliveiratm@gmail.com>
2025-07-26 16:27:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+      }
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
